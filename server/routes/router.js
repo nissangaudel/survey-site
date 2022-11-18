118d99d3fb79b83b32374d70f7d14a830810e0c3
Nishan Gaudel <nissangaudel@gmial.com>
2022-11-17 23:54:14 -0500

    login and register page added

--- server/routes/router.js
+++ server/routes/router.js
@@ -7,5 +7,13 @@ route.get('/', (req, res, next) => {
     res.render('index')
 });
 
+route.get('/login', (req, res, next) => {
+    res.render('pages/login')
+});
+
+route.get('/register', (req, res, next) => {
+    res.render('pages/register')
+})
+
 
 module.exports = route;
